8e32602aef6de7f78dd10221451b61b21b6e5fd5
Jasmine Wiggins <jasminerwiggins@gmail.com>
2015-04-11 18:27:51 -0500

    Displays man on the moon room

--- game.py
+++ game.py
@@ -20,19 +20,21 @@ def main():
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     
     while True:
-        #manOnMoonRoom()
-        #constellation()
+        manOnMoonRoom()
+        constellationRoom()
         dinosaurRoom()
 
 
 def manOnMoonRoom():
+    manOnMoonBackGround = pygame.image.load('manOnTheMoon.jpg')
+    manOnMoonBackGround = pygame.transform.scale(manOnMoonBackGround, (WINDOWWIDTH, WINDOWHEIGHT))
     while True: # main game loop
         for event in pygame.event.get(): # event handling loop
             if event.type == QUIT:
-                terminate()     
-        
+                terminate()
+
         DISPLAYSURF.fill(BGCOLOR)
-        
+        DISPLAYSURF.blit(manOnMoonBackGround, (0, 0))
         pygame.display.update()
         FPSCLOCK.tick(FPS)
         
