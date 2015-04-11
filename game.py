a11821534e89f8b5ee4294ab2f14bb11e1c92ef6
rachelmad <rachelvmadrigal@gmail.com>
2015-04-11 15:24:53 -0500

    Initial commit

---
+++ game.py
@@ -0,0 +1,44 @@
+import random, pygame, sys
+from pygame.locals import *
+
+#Global variables
+FPS = 15                         #Frames per second
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 600
+
+#             R    G    B
+WHITE     = (255, 255, 255)
+BLACK     = (  0,   0,   0)
+RED       = (255,   0,   0)
+BGCOLOR = BLACK
+
+def main():
+    global FPSCLOCK, DISPLAYSURF         #Global variables
+
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    
+    while True:
+        runGame()
+
+
+def runGame():
+    while True: # main game loop
+        for event in pygame.event.get(): # event handling loop
+            if event.type == QUIT:
+                terminate()     
+        
+        DISPLAYSURF.fill(BGCOLOR)
+        
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+		
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
